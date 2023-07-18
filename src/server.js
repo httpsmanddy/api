20abaa23a107025d168bf6d2b92bb5c79babcbe7
amanda <amandashuca@gmail.com>
2023-07-18 19:40:57 -0300

    get request

--- src/server.js
+++ src/server.js
@@ -2,8 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.get("/message", (request, response) => {
-    response.send("hello, world!");
+app.get("/message/:id/:user", (request, response) => {
+    const { id, user } = request.params;
+    
+    response.send(`
+    mensagem ID: ${id}
+    para o usuario${user}
+    `)
 });
 
 const PORT = 3333;
